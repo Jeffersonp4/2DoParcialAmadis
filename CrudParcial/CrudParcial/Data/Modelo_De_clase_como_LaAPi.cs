db1c352c7dfa9741b0068aa722696568ea6eceaa
Jefferson Payano Del Rio <jpayano@pmedia.com>
2021-08-02 16:04:53 -0400

    CrudConBlazor

---
+++ CrudParcial/CrudParcial/Data/Modelo_De_clase_como_LaAPi.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudParcial.Data
+{
+    public class Modelo_De_clase_como_LaAPi
+    {
+    }
+}
---
+++ CrudParcial/CrudParcial/Data/Response.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudParcial.Data
+{
+    public class Response
+    {
+        // Aqui va el mismo modelo de clase que esta en la api
+
+        public int Exito { get; set; }
+        public string Mensaje { get; set; }
+        public object Data { get; set; }
+    }
+}
